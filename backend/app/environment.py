c01992cbc812259e5cfe57579913286a4db833b8
dasbd72 <twbd723@gmail.com>
2025-03-25 10:35:17 +0800

    fix: update allowed origins to use HTTPS in production environment configuration

--- backend/app/environment.py
+++ backend/app/environment.py
@@ -11,9 +11,10 @@ environment = Environment()
 
 if ENV == "production":
     environment.allowed_origins = [
-        "leetcode-progress.s3-website-ap-northeast-1.amazonaws.com",
-        "d2nwgjwnrp77m0.cloudfront.net",
-        "leetcode-progress.dasbd72.com",
+        "https://leetcode-progress.s3-website-ap-northeast-1.amazonaws.com",
+        "http://leetcode-progress.s3-website-ap-northeast-1.amazonaws.com",
+        "https://d2nwgjwnrp77m0.cloudfront.net",
+        "https://leetcode-progress.dasbd72.com",
     ]
 else:
     environment.allowed_origins = ["*"]
