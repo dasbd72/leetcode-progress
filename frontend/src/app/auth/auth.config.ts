0c207f5a2b6e7c704edda057c2b82dcaead81a12
dasbd72 <twbd723@gmail.com>
2025-03-27 23:03:56 +0800

    fix: update redirect and logout URIs to include /auth/callback

--- frontend/src/app/auth/auth.config.ts
+++ frontend/src/app/auth/auth.config.ts
@@ -4,8 +4,8 @@ import { LogLevel, PassedInitialConfig } from 'angular-auth-oidc-client';
 export const authConfig: PassedInitialConfig = {
   config: {
     authority: environment.auth.authority,
-    redirectUrl: window.location.origin,
-    postLogoutRedirectUri: window.location.origin,
+    redirectUrl: window.location.origin + '/auth/callback',
+    postLogoutRedirectUri: window.location.origin + '/auth/callback',
     clientId: environment.auth.clientId,
     scope: 'openid email profile aws.cognito.signin.user.admin',
     responseType: 'code',
@@ -16,7 +16,7 @@ export const authConfig: PassedInitialConfig = {
     // Fixes the issue with the logoff function error
     customParamsEndSessionRequest: {
       client_id: environment.auth.clientId,
-      logout_uri: window.location.origin,
+      logout_uri: window.location.origin + '/auth/callback',
     },
   },
 };
