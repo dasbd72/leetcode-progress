77d30e654be46062777b30678bf813c8e453f603
dasbd72 <twbd723@gmail.com>
2025-03-27 23:36:05 +0800

    fix: integrate auth interceptor with HTTP client in app configuration

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,18 +1,18 @@
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { authConfig } from './auth/auth.config';
 import { LocalSecurityStorage } from './auth/local-security-storage';
-import { AbstractSecurityStorage, provideAuth } from 'angular-auth-oidc-client';
+import { AbstractSecurityStorage, authInterceptor, provideAuth } from 'angular-auth-oidc-client';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAuth(authConfig),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authInterceptor()])),
     { provide: AbstractSecurityStorage, useClass: LocalSecurityStorage },
   ],
 };
