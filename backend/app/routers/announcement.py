a6ac10a0ce5165929beb5e519ac479af00862765
dasbd72 <twbd723@gmail.com>
2025-05-10 22:24:22 +0800

    feat: add announcement for subscribing to other users and viewing their progress

--- backend/app/routers/announcement.py
+++ backend/app/routers/announcement.py
@@ -12,5 +12,10 @@ async def get_announcement():
                 "content": "User system has been implemented!\nLogin and save your LeetCode username to start!",
                 "date": "2025-03-08",
             },
+            {
+                "title": "Subscribe Other Users",
+                "content": "You can now subscribe to other users!\nSee their progress and achievements!",
+                "date": "2025-05-10",
+            },
         ]
     }
